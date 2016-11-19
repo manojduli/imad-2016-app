03dc78b9f13527edc6859891fe225cdce3af71dd
manoj duli <manojduli123@gmail.com>
2016-11-19 17:43:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move the image
+var img = document.getElementById('imadi');
+img.onclick = function () {
+    img.style.marginleft='100px';
+};
