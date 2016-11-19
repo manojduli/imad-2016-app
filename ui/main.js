5c57c65ab6245ac29b22abc62530f3cb85628cc3
manoj duli <manojduli123@gmail.com>
2016-11-19 23:45:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'HI! THIS IS MANOJ DULI';
 
 //Move the Image
-var img = document.getElementById('imad');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
     marginLeft= marginLeft+10;
