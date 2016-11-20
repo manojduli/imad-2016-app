66715fd8887ac36d67d8f3fe0842034edfedb8a2
manoj duli <manojduli123@gmail.com>
2016-11-20 14:39:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,14 @@
-console.log('Loaded!');
-
-//Change the main-text
-var element = document.getElementById('main-text');
-element.innerHTML = 'HI! THIS IS MANOJ DULI';
-
-//Move the Image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft= marginLeft+10;
-    img.style.marginLeft= marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-};   
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick=function(){
+    
     //CREATE A REQUEST OBJECT
     var request =new xmlHttpRequest();
+    
+    //CAPTURE THE REQUEST AND STORE IT IN A VARIABLE
     request.onreadystatechange=function (){
-    if(request.readystate===xmlHttpRequest.DONE){
+      if(request.readystate===xmlHttpRequest.DONE){
         //TAKE ACTION
         if(request.status == 200){
             var counter = request.responsetext;
