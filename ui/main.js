2eb697aec27ffe9131622a9e5f932ff5cd103373
manoj duli <manojduli123@gmail.com>
2016-11-21 09:40:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ submit.onclick=function(){
 	};
      var nameInput=document.getElementById('name');
      var name=nameInput.value;
-	request.open('GET','http://manojduli.imad.hasura-app.io/submit-one?name='+name,true);
+	request.open('GET','http://manojduli.imad.hasura-app.io/submit-name?name='+name,true);
 	request.open('GET','/submit-one?name='+name,true);
 	request.send(null);
 
